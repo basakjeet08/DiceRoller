4fc37686f1b7619c5791968be197694ad0fbd671
Anirban Basak <jeetbasak2002@gmail.com>
2022-10-05 09:57:21 +0530

    05/10/2022

--- app/src/main/java/com/basakjeet08/diceroller/Dice.kt
+++ app/src/main/java/com/basakjeet08/diceroller/Dice.kt
@@ -1,8 +1,23 @@
 package com.basakjeet08.diceroller
 
 class Dice(private val sides:Int) {
+    private val dice1 : Int = R.drawable.dice_1
+    private val dice2 : Int = R.drawable.dice_2
+    private val dice3 : Int = R.drawable.dice_3
+    private val dice4 : Int = R.drawable.dice_4
+    private val dice5 : Int = R.drawable.dice_5
+    private val dice6 : Int = R.drawable.dice_6
     fun roll() : Int{
+        var showImage : Int = 0
         val diceRange = 1..sides
-        return diceRange.random()
+        when(diceRange.random()){
+            1 -> showImage = dice1
+            2 -> showImage = dice2
+            3 -> showImage = dice3
+            4 -> showImage = dice4
+            5 -> showImage = dice5
+            6 -> showImage = dice6
+        }
+        return showImage
     }
 }
--- app/src/main/java/com/basakjeet08/diceroller/MainActivity.kt
+++ app/src/main/java/com/basakjeet08/diceroller/MainActivity.kt
@@ -2,20 +2,19 @@ package com.basakjeet08.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val image: ImageView = findViewById(R.id.imageView)
         val button: Button = findViewById(R.id.button)
-        val output : TextView = findViewById(R.id.textView)
         button.setOnClickListener {
             val dice = Dice(6)
             val randomNumber = dice.roll()
-            output.text = randomNumber.toString()
+            image.setImageResource(randomNumber)
         }
     }
 }
