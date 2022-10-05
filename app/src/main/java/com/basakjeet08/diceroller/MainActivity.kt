53ce92c31b16532c981b0d2dc267f2551b274682
Anirban Basak <jeetbasak2002@gmail.com>
2022-10-05 07:12:26 +0530

    Update MainActivity.kt

--- app/src/main/java/com/basakjeet08/diceroller/MainActivity.kt
+++ app/src/main/java/com/basakjeet08/diceroller/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun roll(){
+        val diceRange = 1..6
+        val randomNumber = diceRange.random()
+        println("$randomNumber")
+
     }
 }
