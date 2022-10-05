acee401fdb01a04cfae928a5484b3f370d403106
Anirban Basak <jeetbasak2002@gmail.com>
2022-10-05 10:47:21 +0530

    05/10/2022

--- app/src/main/java/com/basakjeet08/diceroller/Dice.kt
+++ app/src/main/java/com/basakjeet08/diceroller/Dice.kt
@@ -3,6 +3,8 @@ package com.basakjeet08.diceroller
 import android.widget.ImageView
 
 class Dice(sides:Int) {
+
+    //All the Dice image resources are saved in a variable
     private val dice1 : Int = R.drawable.dice_1
     private val dice2 : Int = R.drawable.dice_2
     private val dice3 : Int = R.drawable.dice_3
@@ -10,20 +12,23 @@ class Dice(sides:Int) {
     private val dice5 : Int = R.drawable.dice_5
     private val dice6 : Int = R.drawable.dice_6
     private val diceRange = 1..sides
+
+    //This function Sets the Image according to the Random Rolled Number and returns the Random Number
     fun roll(image:ImageView) : Int{
         val number = diceRange.random()
-        when(number){
-            1 -> image.setImageResource(dice1)
-            2 -> image.setImageResource(dice2)
-            3 -> image.setImageResource(dice3)
-            4 -> image.setImageResource(dice4)
-            5 -> image.setImageResource(dice5)
-            6 -> image.setImageResource(dice6)
-        }
+
+        // image is Set According to the Random Number
+        image.setImageResource(when(number){
+            1 -> dice1
+            2 -> dice2
+            3 -> dice3
+            4 -> dice4
+            5 -> dice5
+            else -> dice6
+        })
         return number
     }
-    fun checkLuckyNumber(rolledNumber : Int) : Boolean{
-        val luckyNumber = diceRange.random()
-        return (luckyNumber == rolledNumber)
-    }
+
+    //This Function checks if the Rolled Random Number is equal to the Lucky Number and sends the Boolean Value
+    fun checkLuckyNumber(rolledNumber : Int) : Boolean{ return (diceRange.random() == rolledNumber) }
 }
--- app/src/main/java/com/basakjeet08/diceroller/MainActivity.kt
+++ app/src/main/java/com/basakjeet08/diceroller/MainActivity.kt
@@ -10,10 +10,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Creating the Button ImageView and the Dice Class Instances for Later Use
         val image: ImageView = findViewById(R.id.imageView)
         val button: Button = findViewById(R.id.button)
+        val dice = Dice(6)
+
+        // Sets an Random Rolled Dice Image
+        dice.roll(image)
+
+        //When the Button Roll is Pressed this Section Works
         button.setOnClickListener {
-            val dice = Dice(6)
             val rolledNumber = dice.roll(image)
             if(dice.checkLuckyNumber(rolledNumber))
                 Toast.makeText(this, "You Win!!", Toast.LENGTH_SHORT).show()
